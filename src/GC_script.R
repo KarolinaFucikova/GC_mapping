30aa19714c62204dfbfbc55b2bad09a2aa35bab6
Karolina Fucikova <karolina.fucikova@gmail.com>
2023-04-12 13:14:30 -0400

    grayscale plots

--- src/GC_script.R
+++ src/GC_script.R
@@ -45,6 +45,22 @@ fancyplot1<-ggplot(gc_clean, aes(x=max_temp, y=SSU_GC)) +
 
 fancyplot1
 
+# grayscale plotting
+fancyplot2<-ggplot(gc_clean, aes(x=max_temp, y=SSU_GC)) + 
+  geom_smooth(method=lm, se=FALSE, color="gray") +          
+  geom_point(aes(color=habitat, shape=habitat), size=5) +
+  geom_text(aes(label=ifelse(SSU_GC>54,as.character(species),''),hjust=1.2,vjust=1.5)) +
+  scale_color_manual(values=c('#666666','#999999','#333333')) +
+  theme(legend.key.size = unit(2, 'cm'), legend.title = element_text(size=18),
+        legend.text = element_text(size=20),
+        axis.text.x = element_text(size = 15),
+        axis.text.y = element_text(size = 15),
+        axis.title = element_text(size = 20)) + 
+  labs(x = "max temperature at site of origin (°C)",
+       y = "% GC content in 18S gene")
+
+fancyplot2
+
 ## mapping GC content (or other traits) onto a tree
 WJTtree <- "data/cp_nt_concatenated.con.tre"
 WJT<-read.nexus(WJTtree)
